579af40013b1e48ac0b4e2bd7cf21c148dded585
Alyssa Bump <alyssa.bump@gmail.com>
2019-10-25 20:23:28 -0700

    Hopefully Last Push

--- p5/MP3/sketch.js
+++ p5/MP3/sketch.js
@@ -1,5 +1,5 @@
 var cars = [];
-var frogPos ;
+var frogPos;
 var myState = 0;
 var timer = 0;
 var yoda, yodaL, yodaR;
@@ -8,25 +8,33 @@ var birds = [];
 var splashBG, losepic, winpic, playpic;
 var paint;
 var lostsound;
-var hound;
-var hounds = [];
+var hound1, hound2;
 var player;
 var song1;
+var fanfare;
+var x = 0;
+var bottlesLeft = 0;
 
-function preload(){
+function preload() {
   lostsound = loadSound('assets/hit-crunch-distortion-reverb-trail-off_MJxR43Ed.mp3');
   lostsound.stop();
 
   song1 = loadSound("assets/ForATK302_mixdown.mp3");
   song1.loop();
   song1.stop();
+
+  fanfare.loop();
+  fanfare.stop();
 }
+
 
 function setup() {
 
+
+
   createCanvas(800, 800);
 
-paint = loadFont('assets/FingerPaint-Regular.ttf');
+  paint = loadFont('assets/FingerPaint-Regular.ttf');
 
   splashBG = loadImage("assets/ATK3022.png");
   losepic = loadImage("assets/ATK302.4.png");
@@ -45,11 +53,12 @@ paint = loadFont('assets/FingerPaint-Regular.ttf');
   birds[1] = loadImage("assets/bottleatk2.png");
   birds[2] = loadImage("assets/bottleatk3.png");
 
-
+  fanfare = loadSound("assets/hit-crunch-distortion-reverb-trail-off_MJxR43Ed.mp3");
+  // Short_triumphal_fanfare-John_Stracke-815794903.mp3
 
   // hound = loadImage("assets/hound1.png");
-  // hounds[0] = loadImage("assets/hound1.png");
-  // hounds[1] = loadImage("assets/hound2.png");
+  hound1= loadImage("assets/hound1.png");
+  hound2 = loadImage("assets/hound2.png");
 
   //spawn cars
   for (var i = 0; i < 5; i++) {
@@ -60,107 +69,128 @@ paint = loadFont('assets/FingerPaint-Regular.ttf');
   //   hounds.push(new Enemy());
   // }
 
-  frogPos = createVector(width/2, height-80) ;
-  rectMode(CENTER) ;
-  ellipseMode(CENTER) ;
+  frogPos = createVector(width / 2, height - 80);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
   imageMode(CENTER);
 }
 
 function draw() {
 
   //game();
-  switch(myState) {
+  switch (myState) {
 
-  // case 0:
-  // lostsound.stop();
-  // myState=1;
-  // break;
+    // case 0:
+    // lostsound.stop();
+    // myState=1;
+    // break;
 
-  case 0:
-  song1.play();
-  myState=1
-  break;
+    case 0:
+      song1.play();
+      myState = 1
+      break;
 
-  case 1:  // splash screen
-  image(splashBG, width/2, height/2);
-  fill('red');
-  textFont(paint, 20);
-  text('You have sinned against the Mother and now\nyou only have a short amount of time to redeem yourself.\n Go forth now',60, 60);
-  text(35);
-  text('Or face the Hound', 300, 300);
-  textSize(50);
-  text('Click to Begin', width/2, height/2);
-  break;
+    case 1: // splash screen
+      image(splashBG, width / 2, height / 2);
+      fill('red');
+      textFont(paint, 20);
+      text('You have sinned against the Mother and now\nyou only have a short amount of time to redeem yourself.\n Go forth now', 60, 60);
+      textSize(35);
+      text('Or face the Hound', 300, 300);
+      textSize(50);
+      text('Click to Begin', x, height / 2);
+      x +=5;
+        if (x > 800) {
+          x = 0;
+        }
+      break;
 
-  case 2: // the game state
-    game();
-    timer++;
-    if (timer > 600){
-      myState = 5;
-      timer = 0;
-    }
-  break;
+    case 2: // the game state
+      game();
+      timer++;
+      if (timer > 600) {
+        myState = 5;
+        timer = 0;
+      }
+      break;
 
-  case 3: // the win state
-  image(winpic, width/2, height/2);
-  fill('red');
-  textFont(paint, 20);
-  text('You have been redeemed.\n The wrath of the Hound\n will not reach you now.', 450, 300);
-  song1.stop();
-  break;
+      // case 3:
+      // cheersound.play();
+      // myState = 4;
+      // break;
 
-  case 4:
-  song1.stop();
-  myState = 5
-  break;
+    case 3:
+      song1.stop();
+      fanfare.play();
+      myState = 4
+      break;
 
-  case 5:
-  lostsound.play();
-  song1.stop();
-  myState=6
-  break;
+      // case 4:
+      // song1.stop();
+      // myState=5
+      // break;
 
-  case 6: // the lose state
-  // if (myState == 6){
-  //   song1.pause();
-  //   lostsound.play();
-  // }
-  image(losepic, width/2, height/2);
-  fill('white');
-  textFont(paint, 20);
-  text('Nature can be so unforgiving...\n [click to restart]', 60, 700);
-  break;
+    case 4: // the win state
+      image(winpic, width / 2, height / 2);
+      fill('red');
+      textFont(paint, 20);
+      text('You have been redeemed.\n The wrath of the Hound\n will not reach you now.', 450, 300);
+      // song1.stop();
+      break;
 
-// case 5:
-// lostsound.pause();
-// myState = 0;
-// break;
+    case 5:
+      song1.stop();
+      myState = 6
+      break;
+
+    case 6:
+      lostsound.play();
+      song1.stop();
+      myState = 7
+      break;
+
+    case 7: // the lose state
+      // if (myState == 6){
+      //   song1.pause();
+      //   lostsound.play();
+      // }
+      image(losepic, width / 2, height / 2);
+      fill('white');
+      textFont(paint, 20);
+      text('Nature can be so unforgiving...\n [click to restart]', 60, 700);
+      break;
+
+      // case 5:
+      // lostsound.pause();
+      // myState = 0;
+      // break;
   }
 
 
 }
 
-function mouseReleased(){
-  switch(myState){
+function mouseReleased() {
+  switch (myState) {
     case 1:
       myState = 2;
-  break;
+      break;
 
-  case 3: //the win state
-    resetTheGame();
-    myState = 0;
-  break;
+    case 4: //the win state
+      resetTheGame();
+      // cheer.stop();
+      myState = 0;
+      break;
 
-  case 6: //the lose state
-    resetTheGame();
-    lostsound.stop();
-    myState = 0;
-  break;
+    case 7: //the lose state
+      resetTheGame();
+      lostsound.stop();
+      myState = 0;
+      break;
   }
 
-    // if (myState <= 0) {
-    //   myState = 1;
-    // }
+  // if (myState <= 0) {
+  //   myState = 1;
+  // }
 }
 
 // function Enemy(){
@@ -213,9 +243,13 @@ function Car() {
   this.r = random(255);
   this.g = random(255);
   this.b = random(255);
-  this.birdNum = floor(random(birds.length-1));
+  this.birdNum = floor(random(birds.length - 1));
   this.timer = 0;
   this.maxTimer = random(10, 30);
+  this.type = random(1);
+  if (this.type < 0.5){
+    bottlesLeft++;
+  }
 
   // methods
   this.display = function() {
@@ -225,18 +259,23 @@ function Car() {
     // ellipse(this.pos.x+45, this.pos.y+25, 50, 50);
     //image(bird, this.pos.x, this.pos.y, 100, 100);
 
+    if (this.type > 0.5){
+    image(hound1, this.pos.x, this.pos.y, 100, 100) ;
+  } else {
     image(birds[this.birdNum], this.pos.x, this.pos.y, 100, 100);
-
-
-  this.timer++;
-  if (this.timer > 20){
-    this.birdNum = this.birdNum + 1;
-    this.timer = 0;
-
   }
 
+
+
+    this.timer++;
+    if (this.timer > 20) {
+      this.birdNum = this.birdNum + 1;
+      this.timer = 0;
+
+    }
+
     //dont go past the array
-    if (this.birdNum > 2){
+    if (this.birdNum > 2) {
       this.birdNum = 0;
     }
 
@@ -256,7 +295,7 @@ function Car() {
 
 }
 
-function keyPressed(){
+function keyPressed() {
   if (keyCode === LEFT_ARROW) yoda = yodaL;
   if (keyCode === RIGHT_ARROW) yoda = yodaR;
 }
@@ -268,19 +307,26 @@ function checkForKeys() {
   if (keyIsDown(DOWN_ARROW)) frogPos.y = frogPos.y + 5;
 }
 
-function game(){
-  image(playpic, width/2, height/2);
+function game() {
+  image(playpic, width / 2, height / 2);
   for (var i = 0; i < cars.length; i++) {
     cars[i].display();
     cars[i].drive();
     if (cars[i].pos.dist(frogPos) < 50) {
-      cars.splice(i, 1) ;
+      if (cars[i].type > 0.5){
+        myState = 5;
+      } else{
+          cars.splice(i, 1);
+          bottlesLeft--;
+      }
+
     }
-  //   hounds[i].display();
-  //   hounds[i].drive();
+    //   hounds[i].display();
+    //   hounds[i].drive();
   }
 
-  if(cars.length == 0){
+//  if (cars.length == 0) {
+if (bottlesLeft ==0) {
     myState = 3;
   }
   //
@@ -290,22 +336,25 @@ function game(){
 
   // draw the frog
 
-   // fill('green') ;
+  // fill('green') ;
   // ellipse(frogPos.x, frogPos.y, 60, 60) ;
 
   // image(yoda, frogPos.x, frogPos.y, 120, 120);
   image(player, frogPos.x, frogPos.y, 120, 120);
-  checkForKeys() ;
+  textSize(20);
+  text("Bottles Left = "+bottlesLeft, 30, 50);
+  checkForKeys();
 }
 
-function resetTheGame(){
+function resetTheGame() {
   cars = [];
+  bottlesLeft = 0;
   for (var i = 0; i < 5; i++) {
     cars.push(new Car());
   }
-  // hounds = [];
-  // for )var i = 0; i < 1; i++){
-  //   hounds.push(new Enemy());
+  // cars = [];
+  // for (var i = 0; i < 1; i++){
+  //   cars.push(new Car());
   // }
   timer = 0;
 }
