1ca38987c5498b22d30e93cb1063931339a4a005
Alyssa Bump <alyssa.bump@gmail.com>
2019-12-04 16:25:06 -0800

    rename songs

--- p5/MP4/sketch.js
+++ p5/MP4/sketch.js
@@ -16,12 +16,12 @@ function preload() {
   // up = loadImage('assets/OTR.jpg');
   // down = loadImage('assets/KAP.jpg');
 
-  knives = loadSound('Assets/black-veil-bridesknives-and-pens-lyrics-youtubemp3free.org.mp3');
-  caligirls = loadSound('Assets/california-girls-by-katy-perry-ft-snoop-dog-lyrics-youtubemp3free.org.mp3');
+  knives = loadSound('Assets/knives.mp3');
+  caligirls = loadSound('Assets/cal.mp3');
 //  tunak = loadSound('Assets/daler-mehndi-tunak-tunak-tun-video-youtubemp3free.org.mp3');
   // miku = loadSound('Assets/hatsune-miku-ievan-polkka-project-diva-theatre-hd-youtubemp3free.org.mp3');
   // anthem = loadSound('Assets/hymne-natonal-du-canada-en-franais-youtubemp3free.org.mp3');
-   jb = loudSound('Assets/JustinBieber-Despacito(Lyrics)ftLuisFonsiDaddyYankee[Pop].mp3');
+   jb = loudSound('Assets/JBD.mp3');
   // bop = loudSound('Assets/kidz-bop-kids-old-town-road-youtubemp3free.org.mp3');
   // christmas = loadSound('Assets/mariah-carey-all-i-want-for-christmas-is-you-lyrics-youtubemp3free.org.mp3');
   // chum = loadSound('Assets/Weirdrussiansinger-ChumDrumBedrum.mp3');
@@ -32,18 +32,14 @@ function preload() {
 //   createCanvas(windowWidth, windowHeight, WEBGL);
 //   comicsans = loadFont('assets/NotoSansJP-Bold.otf');
 //
-  knives.loop();
-  knives.stop();
-  caligirls.loop();
-  caligirls.stop();
+
 //   tunak.loop();
 //   tunak.stop();
 //   miku.loop();
 //   miku.stop();
 //   anthem.loop();
 //   anthem.stop();
-  jb.loop();
-  jb.stop();
+
 //   bop.loop();
 //   bop.stop();
   // christmas.loop();
@@ -51,104 +47,11 @@ function preload() {
 //   chum.loop();
 //   chum.stop();
 //
-//   anglemode(DEGREES);
-// }
-//
-// function deviceTurned() {
-//   if (turnAxis === 'Y') {
-//     if (value === 0) {
-//       value = 255;
-//     } else if (value === 255) {
-//       value = 0;
-//     }
-//   }
-// }
-//
-// function draw() {
-//   // put drawing code here
-//   background('green');
-//   textFont(comicsans, 30);
-//   fill(250, 237, 39);
-//   fill(value);
-//   rect(25, 25, 50, 50);
-//
-//
-//   switch (direction) {
-//     case 0:
-//       text("Direction: North \nAll I Want For Christmas Is You by Mariah Carey", 100, 500);
-//       image(mariah, 0, 0, 600, 450);
-//       break;
-//
-//     case 1:
-//       textFont(comicsans, 30);
-//       text("Direction: South \nThe Lucky One by Alison Krauss and Union Station", 100, 500);
-//       image(bluegrass, 0, 0, 450, 450);
-//       break;
-//
-//     case 2:
-//       textFont(comicsans, 30);
-//       text("Direction: East \nTunak Tunak Tun by Daler Mehndi", 100, 500);
-//       image(hindi, 0, 0, 650, 450);
-//       break;
-//
-//     case 3:
-//       textFont(comicsans, 30);
-//       text("Direction: West \nCalifornia Girls by Katy Perry", 100, 500);
-//       image(california, 0, 0, 650, 450);
-//       break;
-//
-//     case 4:
-//       textFont(comicsans, 30);
-//       text("Direction: Northeast \nChum Drum Bedrum by Vitas", 100, 500);
-//       image(russian, 0, 0, 450, 450);
-//       break;
-//
-//     case 5:
-//       textFont(comicsans, 30);
-//       text("Direction: Northwest \nFrench Canadian National Anthem", 100, 500);
-//       image(canada, 0, 0, 450, 450);
-//       break;
-//
-//     case 6:
-//       textFont(comicsans, 30);
-//       text("Direction: Southeast \nIevan Polkka sung by Len Kagamine and Many Other Vocaloids", 100, 500);
-//       image(vocaloid, 0, 0, 450, 450);
-//       break;
-//
-//     case 7:
-//       textFont(comicsans, 30);
-//       text("Direction: Southwest \nDespacito by Luis Fonsi, Daddy Yankee, and Justin Bieber", 100, 500);
-//       image(despacito, 0, 0, 650, 450);
-//       break;
-//
-//     case 8:
-//       textFont(comicsans, 30);
-//       text("Direction: Straight Up \nOld Town Road sung by Kidz Bop", 100, 500);
-//       image(up, 0, 0, 450, 450);
-//       break;
-//
-//     case 9:
-//       textFont(comicsans, 30);
-//       text("Direction: Straight Down \nKnives and Pens by Black Veil Brides", 100, 500);
-//       image(down, 0, 0, 450, 450);
-//       break;
-//   }
-//
-// }
 
-// let magSensor = new Magnetometer({
-//   frequency: 60
-// });
-// magSensor.addEventListener('reading', e => {
-//   console.log("Magnetic field along the X-axis " + magSensor.x);
-//   console.log("Magnetic field along the Y-axis " + magSensor.y);
-//   console.log("Magnetic field along the Z-axis " + magSensor.z);
-// });
-// magSensor.start();
 function pauseTheMusic() {
   jb.pause() ;
   knives.pause() ;
-  caligirls.pause() ; 
+  caligirls.pause() ;
 }
 
 var alpha, beta, gamma; // orientation data
@@ -161,6 +64,13 @@ var myState = 0;
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
+
+  knives.loop();
+  knives.stop();
+  caligirls.loop();
+  caligirls.stop();
+  jb.loop();
+  jb.stop();
   // requestT() ;
 
   // initialize accelerometer variables
